d7fd9ff643b916bd9d39185749647a9f8fb9dafa
zitelog <>
2006-01-06 08:33:54 +0000

    *** empty log message ***

--- reaction/src/parser/xmlparse.h
+++ reaction/src/parser/xmlparse.h
@@ -1,9 +1,45 @@
+/***************************************************************************
+ * Simulacro System Administrator SourceForge Project                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   Licensed under the GNU GPL                                            *
+ ***************************************************************************/
+
+/***************************************************************************
+ *      Module Name:   xmlparse.h 
+ *
+ *      Abstract:
+ *
+ *      Revision History:
+ *      Who             When            What
+ *      --------        ----------      -------------------------------
+ *      Name            Date            Modification logs
+ *      Fabio Zito      06-01-2006      First release alpha0.1
+ ***************************************************************************/
+
 #include <stdio.h>
 #include <string.h>
 #include <libxml/xmlwriter.h>
 #include <libxml/parser.h>
 #include <libxml/tree.h>
 #include <libxml/xmlreader.h>
+
+#if defined(LIBXML_WRITER_ENABLED) && defined(LIBXML_OUTPUT_ENABLED) \
+&& defined(LIBXML_READER_ENABLED) && defined(LIBXML_TREE_ENABLED)
 
 #ifndef __PARSE_H
 #define __PARSE_H
@@ -21,14 +57,6 @@
 #define TRUE (!FALSE)
 #endif
 
-#if defined(LIBXML_WRITER_ENABLED) && defined(LIBXML_OUTPUT_ENABLED) \
-&& defined(LIBXML_READER_ENABLED) && defined(LIBXML_TREE_ENABLED)
-#else
-int main(void) {
-    printf(stderr, "Writer or output support not compiled in\n");
-    exit(1);
-}
-#endif
 
 /* Parse/Deparse  */
 typedef struct XmlParseBase
@@ -48,3 +76,9 @@ static int ELEMENT=0;
 
 #endif
 
+#else
+int main(void) {
+    printf(stderr, "Writer or output support not compiled in\n");
+    exit(1);
+}
+#endif
