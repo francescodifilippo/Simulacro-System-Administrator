cab9c5de820d5a2943c6195c35ee1cd12bcd6037
FrancescoDF <21206779+francescodifilippo@users.noreply.github.com>
2025-06-20 11:40:52 +0200

    Fix typo 'Maximazing' -> 'Maximizing'

--- estaconn/remoteconn/base/remoteconn2.c
+++ estaconn/remoteconn/base/remoteconn2.c
@@ -205,7 +205,7 @@ char *ctrl_eod (int fd_expect)
         	{
 			if (VERBOSE)
 			{
-				printf ("- Maximazing timeout window(%d millisec)\n", MAX_BUFFER_TIMEOUT);
+				printf ("- Maximizing timeout window(%d millisec)\n", MAX_BUFFER_TIMEOUT);
 			}
 			tv.tv_sec = 0;
 	       	       	tv.tv_usec = MAX_BUFFER_TIMEOUT;
